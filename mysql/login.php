da6e6e8a0e010c5051e812b391d98cb1f1b45f5e
Keir Staple <keirstaple@comcast.net>
2016-11-18 12:21:30 -0800

    add mysql folder and login page

---
+++ mysql/login.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Login Page</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css" integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+  </head>
+  <body>
+    <div class="container">
+      <div class="col-xs-6">
+        <form action="login.php" method="post">
+          <div class="form-group">
+            <label for="username">Username</label>
+            <input type="text" name="username" class="form-control">
+          </div>
+
+          <div class="form-group">
+            <label for="password">Password</label>
+            <input type="password" name="password" class="form-control">
+          </div>
+
+          <input type="submit" name="submit" value="Submit" class="btn btn-primary">
+        </form>
+      </div>
+    </div>
+  </body>
+</html>
